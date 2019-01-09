7eb8b46b3e5e21b6cc034dfa458f8b707ac65d1f
mattiafioraso <mattia.fioraso@molo17.com>
2019-01-09 17:50:27 +0100

    style: format configurationService unit test

--- packages/09-mobile-phone-case-configurator-web-service/src/data/configurationService.unit.test.js
+++ packages/09-mobile-phone-case-configurator-web-service/src/data/configurationService.unit.test.js
@@ -1,25 +1,30 @@
-import nock from 'nock';
-import createConfigurationService from './configurationService';
 import fetch from 'node-fetch';
+import nock from 'nock';
+
+import createConfigurationService from './configurationService';
 
 describe('create configuration service', () => {
-  const apiUrl = 'https://fake.api';
-
   const configuration = {
     color: '#ff0000',
     texture: 'DOTTED',
     text: 'Dummy text',
   };
 
+  const apiUrl = 'https://fake.api';
+
   beforeAll(() => {
     global.fetch = fetch;
-  });
 
-  describe('load configuration', () => {
     nock(apiUrl)
       .get('/configuration')
       .reply(200, configuration);
 
+    nock(apiUrl)
+      .post('/configuration', configuration)
+      .reply(200, {});
+  });
+
+  describe('load configuration', () => {
     it('should send an API call and return a configuration', () => {
       const configurationService = createConfigurationService(apiUrl);
 
@@ -30,10 +35,6 @@ describe('create configuration service', () => {
   });
 
   describe('save configuration', () => {
-    nock(apiUrl)
-      .post('/configuration', configuration)
-      .reply(200, {});
-
     it('should send an API call and save configuration', () => {
       const configurationService = createConfigurationService(apiUrl);
 
