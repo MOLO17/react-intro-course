1ec4b07da9de1220588dae5da7e821ba62304060
mattiafioraso <mattia.fioraso@molo17.com>
2019-01-10 15:15:28 +0100

    style: integration tests formatting.

--- packages/09-mobile-phone-case-configurator-web-service/src/__tests__/integration/loadConfiguration.integration.test.js
+++ packages/09-mobile-phone-case-configurator-web-service/src/__tests__/integration/loadConfiguration.integration.test.js
@@ -7,25 +7,25 @@ import { loadConfiguration } from '../../store/thunks/thunks';
 import createConfigurationService from '../../data/configurationService';
 
 describe('load configuration integration', () => {
-  const configuration = {
-    color: '#ff0000',
-    texture: 'DOTTED',
-    text: 'Dummy text',
-  };
-
-  const apiUrl = 'https://fake.api';
-
-  global.fetch = fetch;
-
-  nock(apiUrl)
-    .get('/configuration')
-    .reply(200, configuration);
-
-  const configurationService = createConfigurationService(apiUrl);
-
-  const store = configureStore({ configurationService })({});
-
   it('should load remote configuration on load configuration action dispatch', () => {
+    const configuration = {
+      color: '#ff0000',
+      texture: 'DOTTED',
+      text: 'Dummy text',
+    };
+
+    const apiUrl = 'https://fake.api';
+
+    global.fetch = fetch;
+
+    nock(apiUrl)
+      .get('/configuration')
+      .reply(200, configuration);
+
+    const configurationService = createConfigurationService(apiUrl);
+
+    const store = configureStore({ configurationService })({});
+
     return store.dispatch(loadConfiguration()).then(() => {
       expect(store.getState()).toEqual(configuration);
     });
